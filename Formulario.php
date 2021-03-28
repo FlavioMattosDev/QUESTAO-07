096b82eb79eee236f891f54cffd5b777bf0fa721
FlavioMattosDev <80709540+FlavioMattosDev@users.noreply.github.com>
2021-03-28 19:06:18 -0300

    Add files via upload

---
+++ Formulario.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário - Questão 07</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <h3> Cadastro do Produto </h3><hr>
+                <form action="cadastroProduto.php" method="POST">
+                <label> Nome do Produto </label><br>
+                <input type="text" class="form-control" name="nomeProduto" placeholder="Nome"><br><br>
+
+                <label> Quantidade </label><br>
+                <input type="text" class="form-control" name="quantidadeProduto" placeholder="Quantidade"><br><br>
+
+                <label> Preço do Produto </label><br>
+                <input type="text" class="form-control" name="precoProduto" placeholder="Preço"><br><br>
+
+                <button class="btn btn-primary"> Cadastrar Produto </button>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ listaProdutos.php
@@ -0,0 +1,56 @@
+<?php
+    $produtos = file("produtos.txt");
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem - Questão 07</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <hr>
+                <h3> Listagem dos Produtos </h3><hr>
+
+                <table class="table">
+                    <thead>
+                        <th>Nome do Produto</th>
+                        <th>Quantidade</th>
+                        <th>Preço do Produto</th>
+                    </thead>
+                    
+                    <tbody>
+                    <?php
+                        foreach ($produtos as $produto){
+                            $dado = explode(" - ", $produto);
+                    ?>
+
+                        <tr>
+                            <td><?=$dado[0];?></td>
+                            <td><?=$dado[1];?></td>
+                            <td><?=$dado[2];?></td>
+                        </tr>
+
+                    <?php } ?>
+
+                    </tbody>
+                </table>
+                <a href="Formulario.php" class="btn btn-primary"> Cadastrar Produto</a>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+</body>
+</html>
